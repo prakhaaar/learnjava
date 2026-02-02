c66a4a165700a8d34d3a7d8ab82ce412bbe99ad8
Prakhar <mprakhar07@gmail.com>
2026-02-02 14:12:53 +0530

    creating a json style response using java and springbooot

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,11 +3,16 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.HelloResponse;
+
 @RestController
 public class HelloController {
-    @GetMapping("/")
-    public String Hello(){
-        return "hello from spring";
-    }
 
+    @GetMapping("/")
+    public HelloResponse hello() {
+        return new HelloResponse(
+                "Hello from Spring Boot API!",
+                "SUCCESS"
+        );
+    }
 }
